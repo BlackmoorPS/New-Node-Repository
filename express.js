a151e4db1eb78d3cdc97b89aec647507e8c66493
Prasanth Srinivasan <prashanth.srini18@gmail.com>
2018-06-14 10:08:37 +0530

    Adding Bootstrap Styling

--- express.js
+++ express.js
@@ -5,11 +5,11 @@ var app =express();
 hbs.registerPartials(__dirname + '/views/partials');
 hbs.registerHelper('currentYear', ()=>{
   return new Date().getFullYear()
-})
+});
 app.set('view engine', 'hbs');
 app.get('/help', (req,res)=>{
   res.render('help.hbs');
-})
+});
 app.get('/', (req,res)=>{
   res.render('home.hbs');
 });
